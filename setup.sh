2aa145cf2340d5aae2b445e2d2e493eb45ea8a81
sfyklight <72880936+sfyklight@users.noreply.github.com>
2022-05-08 14:38:34 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ apt install tor -y
 service tor start
 service tor stop
 apt install python3 -y
+apt install python3-pip -y
 pip3 install requests[socks]
+pip install requests[socks]
 mv torrc /etc/tor/torrc
 
 
